c991bb179e124a4f71c2ff995b184a4d367a05bb
arbuzaicer <maxpachinov@gmail.com>
2020-02-01 23:11:12 +0200

    Fix problems. Change values inside strings.

--- 01-homework/Js/index.js
+++ 01-homework/Js/index.js
@@ -4,36 +4,13 @@ const inp2 = document.querySelector('#val2');
 const inp3 = document.querySelector('#val3');
 const resBtn = document.getElementById('reset');
 
-document.getElementById('submit').addEventListener('click', getData);
+document.getElementById('submit').addEventListener('click', getDataHandler);
 
-
-function getData(event) {
+function getDataHandler(event) {
     event.preventDefault();
     document.body.style.height = '150%';
     resBtn.classList.remove('hide');
-    const blackTea = +inp1.value;
-    const beef = +inp2.value;
-    const mushrooms = +inp3.value;
-    const total = blackTea + beef + mushrooms;
-    const discount = Math.random().toFixed(2);
-
-    function innerHTML() {
-        document.querySelector('.data').innerHTML = `
-        <div class="resData"> <div class="textField">Максимальное число</div> <div class="values">${Math.max(blackTea, beef, mushrooms)}</div> </div>
-        <div class="resData"> <div class="textField">Минимальное число</div> <div class="values">${Math.min(blackTea, beef, mushrooms)}</div> </div>
-        <div class="resData"> <div class="textField">Стоимость всех товаров</div> <div class="values">${total}</div> </div>
-        <div class="resData"> <div class="textField">Целая часть стоимости всех товаров, округленная в меньшую сторону</div> <div class="values">${Math.floor(blackTea) + Math.floor(beef) + Math.floor(mushrooms)}</div> </div>
-        <div class="resData"> <div class="textField">Сумма товаров округленная до сотен</div> <div class="values">${getRoundedValue(total.toFixed(0))}</div> </div>
-        <div class="resData"> <div class="textField">Сумма всех товаров есть четным (EVEN) или нечетным (ODD) числом</div> <div class="values">${(Math.floor(total) % 2 === 0) ? 'Even' : 'Odd'}</div> </div>
-        <div class="resData"> <div class="textField">Сумма сдачи, при оплате всех товаров (без округления), если клиент платит 500 грн </div> <div class="values">${500 - total + ' UAH'}</div> </div>
-        <div class="resData"> <div class="textField">Среднее значение цен, округленное до второго знака после запятой</div> <div class="values">${((total) / 3).toFixed(2)}</div> </div>
-        <div class="resData"> <div class="textField">Скидка дня составляет</div> <div class="values">${discount * 100}%</div> </div>
-        <div class="resData"> <div class="textField">Сумма товаров с учетом скидки</div> <div class="values">${(total * (1 - discount)).toFixed(2)}</div> </div>
-        <div class="resData"> <div class="textField">Упущенная выгода</div> <div class="values">${(Math.floor(total / 2) - (Math.floor(total) - Math.floor(total) * (1 - discount))).toFixed(2)}</div> </div>
-        `
-    }
-
-    innerHTML();
+    document.querySelector('.data').innerHTML = innerHTML();
 
     resBtn.addEventListener('click', () => {
         document.querySelector('.data').innerHTML = '';
@@ -49,4 +26,37 @@ function getRoundedValue(sum) {
     const lengthValue = value.length;
     const curValue = sum / (10 ** (lengthValue - 1));
     return Math.round(curValue) * 10 ** (lengthValue - 1);
+}
+
+function innerHTML() {
+    /*Variables section*/
+    const blackTea = +inp1.value;
+    const beef = +inp2.value;
+    const mushrooms = +inp3.value;
+    const total = blackTea + beef + mushrooms;
+    const discount = Math.random().toFixed(2);
+    const maxValue = Math.max(blackTea, beef, mushrooms);
+    const minValue = Math.min(blackTea, beef, mushrooms);
+    const averageRounded = Math.floor(blackTea) + Math.floor(beef) + Math.floor(mushrooms);
+    const isEven = (Math.floor(total) % 2 === 0) ? 'Even' : 'Odd';
+    const paymentValue = 500;
+    const restValue = paymentValue - total + ' UAH';
+    const averageValue = ((total) / 3).toFixed(2);
+    const randomDiscount = discount * 100;
+    const totalDiscountPrice = (total * (1 - discount)).toFixed(2);
+    const profit = (Math.floor(total / 2) - (Math.floor(total) - Math.floor(total) * (1 - discount))).toFixed(2);
+
+    return `
+        <div class="resData"> <div class="textField">Максимальное число</div> <div class="values">${maxValue}</div> </div>
+        <div class="resData"> <div class="textField">Минимальное число</div> <div class="values">${minValue}</div> </div>
+        <div class="resData"> <div class="textField">Стоимость всех товаров</div> <div class="values">${total}</div> </div>
+        <div class="resData"> <div class="textField">Целая часть стоимости всех товаров, округленная в меньшую сторону</div> <div class="values">${averageRounded}</div> </div>
+        <div class="resData"> <div class="textField">Сумма товаров округленная до сотен</div> <div class="values">${getRoundedValue(total.toFixed(0))}</div> </div>
+        <div class="resData"> <div class="textField">Сумма всех товаров есть четным (EVEN) или нечетным (ODD) числом</div> <div class="values">${isEven}</div> </div>
+        <div class="resData"> <div class="textField">Сумма сдачи, при оплате всех товаров (без округления), если клиент платит 500 грн </div> <div class="values">${restValue}</div> </div>
+        <div class="resData"> <div class="textField">Среднее значение цен, округленное до второго знака после запятой</div> <div class="values">${averageValue}</div> </div>
+        <div class="resData"> <div class="textField">Скидка дня составляет</div> <div class="values">${randomDiscount}%</div> </div>
+        <div class="resData"> <div class="textField">Сумма товаров с учетом скидки</div> <div class="values">${totalDiscountPrice}</div> </div>
+        <div class="resData"> <div class="textField">Упущенная выгода</div> <div class="values">${profit}</div> </div>
+        `
 }
