90ea180bea843261a726db583eddb494f6e2a2b0
arbuzaicer <maxpachinov@gmail.com>
2020-02-17 17:05:33 +0200

    Change images link

--- 06-homework/js/index.js
+++ 06-homework/js/index.js
@@ -10,8 +10,8 @@ const studentSingle = students.map((stud, i) => {
     <div class="front-card">
         <div class="user-info">
     <img class="user-logo" src=${stud.gender === 'female'
-        ? './images/female-user-icon.png'
-        : './images/male-user-icon.png'} alt="user-icon"+${i}>
+        ? 'https://icons-for-free.com/iconfiles/png/512/female+person+user+woman+young+icon-1320196266256009072.png'
+        : 'https://i.pinimg.com/originals/af/25/49/af25490494d3338afef00869c59fdd37.png'} alt="user-icon">
     <h2 class="user-name">${stud.name}</h2>
 </div>
 </div>
