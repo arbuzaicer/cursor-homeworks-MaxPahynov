daca8c0cfcf9a1385ce2340e86a95bfcd999b922
arbuzaicer <maxpachinov@gmail.com>
2020-03-03 18:40:22 +0200

    Change BG for guitar; Add string image for strings; Add css reactions on string active

--- 10-homework/js/index.js
+++ 10-homework/js/index.js
@@ -1,9 +1,10 @@
 class Guitar {
     constructor() {
         this.guitarContainer = document.querySelector('.guitar');
-        this.strings = document.querySelectorAll('.string');
+        this.strings = document.querySelectorAll('.string-single');
         this.sounds = document.querySelectorAll('.sound');
         this.stringsButtons = ["Numpad6", "Numpad5", "Numpad4", "Numpad3", "Numpad2", "Numpad1"];
+        this.isStringActive = false;
     }
 
     _play() {
@@ -16,6 +17,12 @@ class Guitar {
             if (event.target === this.strings[i]) {
                 this.strings[i].addEventListener('mouseleave', () => {
                     this.stringSound(i);
+                    this.isStringActive = true;
+                    this.isStringActiveFunc(i);
+                    setTimeout(() => {
+                        this.isStringActive = false;
+                        this.isStringActiveFunc(i);
+                    }, 250)
                 })
             }
         }
@@ -25,6 +32,12 @@ class Guitar {
         for (let i = 0; i < this.stringsButtons.length; i++) {
             if (event.code === this.stringsButtons[i]) {
                 this.stringSound(i);
+                this.isStringActive = true;
+                this.isStringActiveFunc(i);
+                setTimeout(() => {
+                    this.isStringActive = false;
+                    this.isStringActiveFunc(i);
+                }, 250)
             }
         }
     }
@@ -32,6 +45,15 @@ class Guitar {
     stringSound(iter) {
         this.sounds[iter].currentTime = 0;
         this.sounds[iter].play();
+    }
+
+    isStringActiveFunc(iter) {
+        if (this.isStringActive) {
+            this.strings[iter].classList.add('active');
+        }
+        if (!this.isStringActive) {
+            this.strings[iter].classList.remove('active')
+        }
     }
 }
 
