f06e0b95e8833ba16109b16d12832ebc8ff3452c
arbuzaicer <maxpachinov@gmail.com>
2020-03-03 09:44:14 +0200

    Change sound logic

--- 10-homework/js/index.js
+++ 10-homework/js/index.js
@@ -1,34 +1,16 @@
 /*Variables section*/
 
-const string1 = document.getElementById('string1');
-const string2 = document.getElementById('string2');
-const string3 = document.getElementById('string3');
-const string4 = document.getElementById('string4');
-const string5 = document.getElementById('string5');
-const string6 = document.getElementById('string6');
+const strings = document.querySelectorAll('.string');
+const sounds = document.querySelectorAll('.sound');
 
-string1.addEventListener('mouseenter', ()=> {
-    const currentSound = document.getElementById('sound1');
-    currentSound.play();
+/*Listener*/
+
+window.addEventListener('mousemove', (e) => {
+    for (let i = 0; i < strings.length; i++) {
+        if (e.target === strings[i]) {
+            sounds[i].currentTime = 0;
+            sounds[i].play();
+        }
+    }
 });
 
-string2.addEventListener('mouseenter', ()=> {
-    const currentSound = document.getElementById('sound2');
-    currentSound.play();
-});
-string3.addEventListener('mouseenter', ()=> {
-    const currentSound = document.getElementById('sound3');
-    currentSound.play();
-});
-string4.addEventListener('mouseenter', ()=> {
-    const currentSound = document.getElementById('sound4');
-    currentSound.play();
-});
-string5.addEventListener('mouseenter', ()=> {
-    const currentSound = document.getElementById('sound5');
-    currentSound.play();
-});
-string6.addEventListener('mouseenter', ()=> {
-    const currentSound = document.getElementById('sound6');
-    currentSound.play();
-});
